f6023110edc9b1a5914e8b157cce4e1870493340
Phi Quoc Tu Son <phiquoctuson2004@gmail.com>
2025-11-03 14:53:32 +0700

    Update Layout bottom nav menu

--- app/src/main/java/com/example/leafyapp/MainActivity.kt
+++ app/src/main/java/com/example/leafyapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.leafyapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -15,21 +14,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home,
+                R.id.navigation_garden,
+                R.id.navigation_camera,
+                R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/leafyapp/ui/camera/CameraViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.leafyapp.ui.camera
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CameraViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Camera Fragment"
+    }
+    val text: LiveData<String> = _text
+}
